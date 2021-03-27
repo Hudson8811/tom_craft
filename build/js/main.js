4a63e943411824bc2bcb916fad3c9669fa2c1f37
hudson8811 <hudson88@yandex.ru>
2021-03-27 11:08:04 +0300

    changed

--- build/js/main.js
+++ build/js/main.js
@@ -30,13 +30,22 @@ $(document).ready(function(){
     $('.card-slider-main').slick({
         dots: false,
         infinite: false,
-        speed: 500,
         arrows: true,
         speed: 300,
         variableWidth: true,
         appendArrows:$('.card'),
         prevArrow:'<svg width="22" height="40" viewBox="0 0 22 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.761257 19.2026L19.6306 0.333211C20.0749 -0.11107 20.7874 -0.11107 21.2317 0.333211C21.676 0.777493 21.676 1.49002 21.2317 1.9343L3.16708 19.999L21.2317 38.0636C21.676 38.5079 21.676 39.2204 21.2317 39.6647C21.0138 39.8826 20.7204 40 20.4354 40C20.1504 40 19.857 39.891 19.639 39.6647L0.769638 20.7953C0.325356 20.3594 0.325359 19.6385 0.761257 19.2026Z" fill="#999999"/></svg>',
         nextArrow:'<svg width="22" height="40" viewBox="0 0 22 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.2387 19.2026L2.36936 0.333211C1.92508 -0.11107 1.21255 -0.11107 0.76827 0.333211C0.323988 0.777493 0.323988 1.49002 0.76827 1.9343L18.8329 19.999L0.76827 38.0636C0.323988 38.5079 0.323988 39.2204 0.76827 39.6647C0.986219 39.8826 1.27961 40 1.56462 40C1.84963 40 2.14303 39.891 2.36098 39.6647L21.2304 20.7953C21.6746 20.3594 21.6746 19.6385 21.2387 19.2026Z" fill="#666666"/></svg>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: false,
+              }
+            }
+          ]
     });
 
     $('.card-modal-slider').slick({
